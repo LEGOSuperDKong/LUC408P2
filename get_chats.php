3cbee7a4a5fcdaf4e5ac291d2aedf97bddb98bb1
Patrick Robbins <k@167141162.com>
2018-02-15 14:59:58 -0500

    Add files via upload

--- get_chats.php
+++ get_chats.php
@@ -12,7 +12,7 @@ $arr = explode("☺",$line);
 $first = 1;
 if (strpos($arr[2], '%'.$id.'%') !== false) {
     if ($first == 1){
-        echo '<button type="button" style="margin-left:10px;margin-bottom:10px;" title="'.str_replace("%",",",$arr[2]).'" class="btn btn-secondary" onclick="switchchat('.$arr[0].')">';
+        echo '<button type="button" style="margin-left:10px;margin-bottom:10px;" title="'.substr(str_replace("%",",",$arr[2]), 1, -1).'" class="btn btn-secondary" onclick="switchchat('.$arr[0].')">';
         echo $arr[1];
         echo "</button><br/>";
         $first = 0;
